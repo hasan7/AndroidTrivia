fd498c1992fe0f962ccd12cb2b9a4fcc4de0d763
hasan <hasan14155@gmail.com>
2022-02-21 06:32:19 +0300

    added Navigation component and its necessary files, added context to the fragments to fix preview unavailable in navigation

--- app/src/main/java/com/example/androidtrivia/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/androidtrivia/navigation/TitleFragment.kt
@@ -1,10 +1,12 @@
-package com.example.androidtrivia
+package com.example.android.navigation
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.example.androidtrivia.R
 import com.example.androidtrivia.databinding.FragmentTitleBinding
 
 
@@ -12,12 +14,17 @@ class TitleFragment : Fragment() {
 
     private lateinit var binding: FragmentTitleBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                                savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         binding = FragmentTitleBinding.inflate(inflater, container, false)
+
+        binding.playButton.setOnClickListener {view : View ->
+            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        }
+
+
+
         return binding.root
     }
 }
